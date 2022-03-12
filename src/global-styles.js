4bd1d1e5e6d2c6c6e8e44b6a3ff6ba0aad15e88f
mathemartins <mathegeniuse@gmail.com>
2022-03-12 17:25:06 +0100

    mod for ysp:

---
+++ src/global-styles.js
@@ -0,0 +1,10 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+     html, body {
+        font-family: 'Gilroy-Regular', Gilroy-Regular, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        font-size: 14px;
+     }
+`;
